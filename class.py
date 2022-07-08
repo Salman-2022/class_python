59271d250ebb53d6210d65981660427143945801
Salman-2022 <108619289+Salman-2022@users.noreply.github.com>
2022-07-08 05:36:46 +0530

    Update class.py

--- class.py
+++ class.py
@@ -22,3 +22,26 @@ student2.marks = 38
 
 did_pass = student2.check()
 print(did_pass)
+
+#second code
+
+
+class Student:
+    def check(something):
+        if something.marks >=40:
+            return True
+        else:
+            return False
+
+    def __init__(something,name,marks):
+        something.name = name
+        something.marks = marks
+
+
+student1 = Student('Harry', 85)
+did_pass = student1.check()
+print(did_pass)
+
+student2 = Student('jannet',38)
+did_pass = Student.check()
+print(did_pass)
